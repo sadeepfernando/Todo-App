2fb791022c07f4f33ac699a13d9185c1ff367271
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-05-10 13:35:09 +0530

    database connection established

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
 const express = require('express');
-
+const mongoose = require('mongoose');
 const PORT = 8000;
 
 
 //initialize express instance
 const app = express();
+
+//connecting databade
+const connectionUrl = "mongodb://localhost:27017/todo";
+
+mongoose.connect(connectionUrl)
+.then(() => {console.log('Database connected successfully')})
+.catch((error) => {console.log(error.message)});
 
 //setup view engine
 app.set('view engine','ejs');
